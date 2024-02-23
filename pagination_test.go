78a7602899ea5297a8b58e773e3dd9cb2071309c
val1nna <val1nna.c@gmail.com>
2024-02-23 11:40:07 +0800

    init commit

--- pagination_test.go
+++ pagination_test.go
@@ -1,1 +1,36 @@
 package pagination
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"testing"
+)
+
+var Db *gorm.DB
+
+type User struct {
+	ID       int    `gorm:"primary_key"`
+	Username string `gorm:"type:varchar(100);not null"`
+}
+
+func connectDb() {
+	dsn := "root:root@tcp(127.0.0.1:3306)/test?charset=utf8mb4&parseTime=True&loc=Local"
+	Db, _ = gorm.Open(mysql.Open(dsn), &gorm.Config{
+		SkipDefaultTransaction: true,
+	})
+}
+
+func TestPaginate(t *testing.T) {
+	connectDb()
+
+	var users []User
+
+	paginator := Paginate(&Option{
+		DB:      Db,
+		Page:    1,
+		Limit:   10,
+		ShowSQL: true,
+	}, &users)
+
+	t.Log(paginator)
+}
